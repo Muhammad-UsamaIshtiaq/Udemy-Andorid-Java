1272bace716670f73253daa3bb7373ee55af8593
Muhammad-UsamaIshtiaq <musama186@gmail.com>
2023-03-16 14:38:23 +0500

    udemy-andorid-java

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Udemy-Andorid-Java
+# Desenvolvimento de Aplicativos Android usando Java
+
+Este repositório é referente ao curso da [DevMasterTeam](http://www.devmasterteam.com/) disponível na plataforma [Udemy](https://www.udemy.com/course/curso-desenvolvedor-java-android/?referralCode=D9561E200493EE0B9792).
+
+**DevMasterTeam**
---
+++ Secao 06 - Java conceitos iniciais/Fonte/src/Comentarios.java
@@ -0,0 +1,17 @@
+/**
+ * Comentários servem somente para entendimento do programa.
+ * São desconsiderados no momento da compilação.
+ * */
+public class Comentarios {
+    public static void main(String[] args) {
+
+        // Comentário de única linha
+        System.out.println("Texto comentado.");
+
+        /*
+        Comentários de várias linhas.
+        Quantas quiser.
+        Para documentar ou explicar.
+        */
+    }
+}
